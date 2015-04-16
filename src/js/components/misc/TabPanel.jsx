05185d9be75b48a12d29bea845f94952da0c1f6a
Richard Olsson <r@richardolsson.se>
2015-04-16 18:32:17 +0200

    Implement simple TabPanel.

---
+++ src/js/components/misc/TabPanel.jsx
@@ -0,0 +1,69 @@
+import React from 'react/addons';
+
+
+export default class TabPanel extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            selectedIndex: 0
+        };
+    }
+
+    componentWillMount() {
+        var i;
+
+        for (i=0; i<this.props.children.length; i++) {
+            if (this.props.children[i].props.selected) {
+                this.setState({
+                    selectedIndex: i
+                });
+                return;
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className="tabpanel">
+                <ul className="tabpanel-tabs">
+                {this.props.children.map(function(tab, index) {
+                    var title = tab.props.tabTitle;
+                    var panel = this;
+                    var onClick = function() {
+                        panel.select(index);
+                    };
+
+                    return <li key={ index } onClick={ onClick }>{ title }</li>;
+                }, this)}
+                </ul>
+
+                <div className="tabpanel-content">
+                {this.props.children.map(function(tab, index) {
+                    if (index === this.state.selectedIndex) {
+                        return (
+                            <div key={ index }>
+                                { tab.props.children }
+                            </div>
+                        );
+                    }
+                }, this)}
+                </div>
+            </div>
+        );
+    }
+
+    select(index) {
+        this.setState({
+            selectedIndex: index
+        });
+    }
+}
+
+export class Tab extends React.Component {
+}
+
+Tab.propTypes = {
+    selected: React.PropTypes.boolean,
+    tabTitle: React.PropTypes.string.isRequired
+};
